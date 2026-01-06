06f53774539aadd470e69ef5c95b9c238b86acdf
ayanakkabi1 <aynour200609@gmail.com>
2026-01-06 14:45:43 +0100

    feat(entity): add Article entity class

---
+++ classes/blog/Article.php
@@ -0,0 +1,54 @@
+//CREATE TABLE articles(
+    id_article int AUTO_INCREMENT PRIMARY KEY,
+    id_theme int NOT NULL,
+    id_client int NOT NULL,
+    titre_article varchar(50) NOT NULL,
+    contenu varchar(5000),
+    tags varchar(200),
+    date_publication DATE,
+    status BOOLEAN DEFAULT TRUE,
+
+   FOREIGN KEY (id_theme) REFERNCES themes(id_theme),
+   FOREIGN KEY (id_client) REFERNCES users(id)
+),	
+<?php
+class Article{
+
+    private $id_article;
+    private $titre_article;
+    private $contenu;
+    private $status;
+    private $tags;
+    private $id_theme;
+    private $id_client;
+
+    public function __construct($id_article,$id_theme,$id_client,$titre_article, $contenu,$tags,$status = TRUE)
+    {   
+        $this->id_article=$id_article;
+        $this->id_theme=$id_theme;
+        $this->id_client=$id_client;
+        $this->titre_article=$titre_article;
+        $this->contenu=$contenu;
+        $this->tags=$tags;
+        $this->status=$status;
+    }
+    public function __get($att)
+    {
+         if (property_exists($this, $att)) {
+            return $this->$att;
+        }
+        return null;
+    }
+    public function __set($att, $value){
+          if (!property_exists($this, $att)) {
+            return false;
+        }
+        if ($att === 'status') {
+            $value = (bool) $value;
+        }
+
+        $this->$att = $value;
+    }
+    
+}
+?>
