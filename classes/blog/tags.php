ab16bf47c582101eec1a8bb83377d210bb56d237
ayanakkabi1 <aynour200609@gmail.com>
2026-01-08 11:30:49 +0100

    feat(php): add Tag entity with magic getters and setters

--- classes/blog/tags.php
+++ classes/blog/tags.php
@@ -1,5 +1,25 @@
+
 <?php
-class tags{
-    
+class Tag{
+    private $id_tags;
+    private $titre_tags;
+    public function __construct($titre_tags)
+    {
+        $this->titre_tags=$titre_tags;
+    }
+       public function __get(string $name) {
+        if (property_exists($this, $name)) {
+            return $this->$name;
+        }
+       return null;
+    }
+
+  
+    public function __set(string $name, $value) {
+    if (property_exists($this, $name)) {
+        $this->$name = $value;
+    }
+}
+
 }
 ?>
