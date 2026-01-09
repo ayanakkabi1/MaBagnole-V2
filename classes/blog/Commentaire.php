30f2a136ccec9594fb0f74e19741466c0d364a6e
ayanakkabi1 <aynour200609@gmail.com>
2026-01-09 16:18:04 +0100

    feat(commentaire): add edit method in comments entity

--- classes/blog/Commentaire.php
+++ classes/blog/Commentaire.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace Blog;
-use PDO;	
-class Commentaire{
+
+use PDO;
+
+class Commentaire
+{
 
     private $id_commentaire;
     private $id_client;
@@ -10,23 +14,24 @@ class Commentaire{
     private $contenu_com;
     private $date_commentaire;
     private $soft_deleted;
-    public function __construct($id_client,$id_article,$titre_com, $contenu_com,$soft_deleted = false)
+    public function __construct($id_client, $id_article, $titre_com, $contenu_com, $soft_deleted = false)
     {
-        $this->id_client=$id_client;
-        $this->id_article=$id_article;
-        $this->titre_com=$titre_com;
-        $this->contenu_com=$contenu_com;
-        $this->soft_deleted=$soft_deleted;
+        $this->id_client = $id_client;
+        $this->id_article = $id_article;
+        $this->titre_com = $titre_com;
+        $this->contenu_com = $contenu_com;
+        $this->soft_deleted = $soft_deleted;
     }
     public function __get($att)
     {
-         if (property_exists($this, $att)) {
+        if (property_exists($this, $att)) {
             return $this->$att;
         }
         return null;
-    }                                                                                  
-    public function __set($att, $value){
-          if (!property_exists($this, $att)) {
+    }
+    public function __set($att, $value)
+    {
+        if (!property_exists($this, $att)) {
             return false;
         }
         if ($att === 'soft_deleted') {
@@ -34,39 +39,42 @@ class Commentaire{
         }
         $this->$att = $value;
     }
-    
-    public static function listerParArticle($pdo, $idArticle){
-        $sql="Select * FROM commentaires where id_article=:idArticle";
+
+    public static function listerParArticle($pdo, $idArticle)
+    {
+        $sql = "Select * FROM commentaires where id_article=:idArticle";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([':idArticle' => $idArticle]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    public function ajouter($pdo) {
-    $sqlInsert = "INSERT INTO commentaires (id_client, id_article, titre_com, contenu_com, date_commentaire, soft_deleted) 
+    public function ajouter($pdo)
+    {
+        $sqlInsert = "INSERT INTO commentaires (id_client, id_article, titre_com, contenu_com, date_commentaire, soft_deleted) 
                   VALUES (:id_client, :id_article, :titre, :contenu, NOW(), 0)";
-    
-    $stmt = $pdo->prepare($sqlInsert);
-    $success = $stmt->execute([
-        ':id_client'  => $this->id_client,
-        ':id_article' => $this->id_article,
-        ':titre'      => $this->titre_com,
-        ':contenu'    => $this->contenu_com
-    ]);
+
+        $stmt = $pdo->prepare($sqlInsert);
+        $success = $stmt->execute([
+            ':id_client'  => $this->id_client,
+            ':id_article' => $this->id_article,
+            ':titre'      => $this->titre_com,
+            ':contenu'    => $this->contenu_com
+        ]);
     }
     public static function listerParClient(PDO $pdo, int $id_client): array
-{
-    $sql = "SELECT *
+    {
+        $sql = "SELECT *
             FROM commentaires
             WHERE id_client = :id_client
             AND soft_deleted = 0
             ORDER BY date_commentaire DESC";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([
-        ':id_client' => $id_client
-    ]);
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            ':id_client' => $id_client
+        ]);
 
-    return $stmt->fetchAll(PDO::FETCH_OBJ);
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+    public static function modifierCommentaire(Pdo $pdo, int $id_client) {
+        $sql="UPDATE commentaires set titre_com=:titre,contenu=:contenu where id_commentaires=:id_client" ;
+    }
 }
-
-}
-?>
